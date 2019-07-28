bc2b53beaf5cee2a71a03afd32f55a0ba424d9ff
Vishal Kumar <vishalonline@outlook.com>
2019-07-28 13:42:17 +0530

    Added method for showAlert

--- app.js
+++ app.js
@@ -40,12 +40,56 @@ class UI {
 
     list.appendChild(row);
   }
+
+  // Remove books from the list method
+  static deleteBook(el) {
+    if (el.classList.contains("delete")) {
+      el.parentElement.parentElement.remove();
+    }
+  }
+
+  static showAlert(message, className) {
+    const div = document.createElement("div");
+    div.className = `alert alert-${className}`;
+    div.appendChild(document.createTextNode(message));
+    const container = document.querySelector(".container");
+    const form = document.querySelector("#book-form");
+    container.insertBefore(div, form);
+  }
+  // Clear field after submit
+  static clearFields() {
+    document.querySelector("#title").value = "";
+    document.querySelector("#author").value = "";
+    document.querySelector("#isbn").value = "";
+  }
 }
 
 //Storage Class: Handles storage
 
 //Event: Display Books
 document.addEventListener("DOMContentLoaded", UI.displayBooks);
+
 //Event: Add a book
+document.querySelector("#book-form").addEventListener("submit", e => {
+  // Prevent actual submit
+  e.preventDefault();
+  //Get form values
+  const title = document.querySelector("#title").value;
+  const author = document.querySelector("#author").value;
+  const isbn = document.querySelector("#isbn").value;
+
+  if (title === "" || author === "" || isbn === "") {
+    alert("Please fill all fields");
+  } else {
+    //Instantiate Book
+    const book = new Book(title, author, isbn);
+
+    UI.addBookToList(book);
+    UI.clearFields();
+  }
+});
 
 //Event: Remove a book
+document.querySelector("#book-list").addEventListener("click", e => {
+  UI.deleteBook(e.target);
+});
